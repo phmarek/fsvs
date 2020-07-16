f6be08db790dfa971d8a15a1424f0243828d82c8
Philipp Marek <philipp@marek.priv.at>
2020-07-16 19:22:13 +0200

    Migrate to github, WIP

--- src/dev/make_fsvs_release.pl
+++ src/dev/make_fsvs_release.pl
@@ -6,11 +6,9 @@ $version=shift() || die "Welche Version??\n";
 $version =~ m#^(\d+\.)+\d+$# || die "Version ungültig!!\n";
 
 
-$url="http://fsvs.tigris.org/svn/fsvs";
 $tagdir="fsvs-$version";
-$tagurl="$url/tags/$tagdir";
 
-system("svn cp -m 'Tagging $version' $url/branches/fsvs-1.2.x/ $tagurl");
+system("git tag '$tagdir'");
 warn "Fehler $? beim Taggen!" if $?;
 
 #print "Getaggt!! Warte auf Bestätigung.\n"; $_=<STDIN>;
@@ -20,37 +18,38 @@ $tempdir="/tmp/" . $$ . ".tmp.".rand();
 
 mkdir ($tempdir) || die "mkdir($tempdir): $!";
 
-sub C { system("rm -rf $tempdir"); };
+sub C { system("rm -rf '$tempdir'"); };
 
 $SIG{"__DIE__"}=sub { print @_; C(); exit($! || 1); };
 
 
-system("svn export $tagurl/fsvs $tempdir/$tagdir");
-#system("svn export $url/trunk/fsvs $tempdir/$tagdir");
+system("git archive --prefix '$tagdir/' | tar -xf -C '$tempdir'");
 die "Fehler $?" if $?;
 
 chdir($tempdir);
 system("cd $tagdir && autoconf");
 if ($?)
 {
-#die "Fehler $?" if $?;
-print "Fehler $?!!\n";
-system("/bin/bash");
+	#die "Fehler $?" if $?;
+	print "Fehler $?!!\n";
+	system("/bin/bash");
 }
 
-open(CH, "< $tagdir/CHANGES") || die $!;
-open(CHHTML,"> CHANGES.html") || die $!;
-while(<CH>)
-{
-chomp;
-	last if /^\s*$/;
+# open(CH, "< $tagdir/CHANGES") || die $!;
+# open(CHHTML,"> CHANGES.html") || die $!;
+# while(<CH>)
+# {
+# 	chomp;
+# 	last if /^\s*$/;
+# 
+# 	print(CHHTML "<B>$_</B>\n<ul>\n"), next if (/^\w/);
+# 	s#^- #<li>#;
+# 	print CHHTML $_, "\n";
+# }
+# print CHHTML "</ul>\n";
+# close CH; close CHHTML;
 
-	print(CHHTML "<B>$_</B>\n<ul>\n"), next if (/^\w/);
-	s#^- #<li>#;
-	print CHHTML $_, "\n";
-}
-print CHHTML "</ul>\n";
-close CH; close CHHTML;
+chdir($tempdir);
 
 system("tar -cvf $tagdir.tar $tagdir");
 die "Fehler $?" if $?;
@@ -61,6 +60,8 @@ system("gzip -v9 $tagdir.tar");
 die "Fehler $?" if $?;
 
 system("md5sum *.tar.* > MD5SUM");
+die "Fehler $?" if $?;
+system("sha256sum *.tar.* > SHA256SUM");
 die "Fehler $?" if $?;
 
 print "ok\n\n   cd $tempdir\n\n";
