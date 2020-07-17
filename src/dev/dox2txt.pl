aeca11efdbec660b40243ff5bef46ee4ba8fe7fc
Philipp Marek <philipp@marek.priv.at>
2020-07-17 11:24:07 +0200

    Change doxygen output path, and fix formatting for newer versions.

--- src/dev/dox2txt.pl
+++ src/dev/dox2txt.pl
@@ -1,18 +1,36 @@
 #!/usr/bin/perl
 
+use open qw(:std :utf8);
+use IPC::Open2;
+
 $input=shift;
 $output=shift;
-open(STDIN, "lynx -dump -nolist -nonumbers $input |") || die $!;
+
+$pid = open2(my $r, my $w, qw"lynx -dump -nolist -nonumbers -stdin") or die $!;
+binmode($r, "encoding(UTF-8)");
+binmode($w, "encoding(UTF-8)");
+
+open(STDIN, "<", $input) or die $!;
+binmode(STDIN, "encoding(UTF-8)");
+while (<STDIN>)
+{
+# There's no space around <code> anymore.
+	s,(\S)(\<code\>),$1 $2,g;
+	print $w $_;
+}
+close $w;
+
 #open(STDOUT, "> $output") || die $!;
 
 # Cut until first <h2> header
-while (<STDIN>)
+while (<$r>)
 {
 # I'd thought lynx had an option to not print these?
 # yes ... -nonumbers.
 	s#\[\d+\]##;
 	next if m#^\[#;
 
+	s/\xe2/--/g; 
 
 #	$p=m#^SYNOPSIS# .. m#^\s*-{30,}#;
 	$p=m#^\w# .. m#^\s*_{30,}#;
